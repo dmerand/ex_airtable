769a73c90c1939fac289271394021cdaeaec775a
Donald Merand <dmerand@explo.org>
2020-10-02 14:41:02 -0400

    add helper methods

--- lib/ex_airtable.ex
+++ lib/ex_airtable.ex
@@ -1,27 +1,100 @@
 defmodule ExAirtable do
   @moduledoc """
-  Provides an interface to query Airtable bases/tables, and an optional server to cache the results of a table into memory for faster access and to avoid API access limitations.
+  Provides an interface to query Airtable bases/tables, and an optional server to cache the results of a table into memory for faster access and to avoid Airtable API access limitations.
+
+  ## Setup
 
   Generally, you'll need to do two things:
 
-  1. Add a `%ExAirtable.Config.Base` into your application configuration for each Airtable base that you intend to reference.
-  2. For each table within a base that you intend to query, define a module in your codebase that implements the `ExAirtable.Table` behaviour (by running `use ExAirtable.Table` and implementing the `table_name()` callback).
+  1. Add a `%ExAirtable.Config.Base{}` into your application configuration for each Airtable base that you intend to reference.
+  2. For each table within a base that you intend to query, define a module in your codebase that implements the `ExAirtable.Table` behaviour (by running `use ExAirtable.Table` and implementing the `base()` and `table_name()` callbacks).
 
-  If you wish to run a local caching server, you'll need to take the third step of defining one module implementing the `ExAirtable.Cache` behaviour for each `Table` you wish to cache.
+  For example:
+
+      defmodule MyApp.MyAirtable do
+        use ExAirtable.Table
+
+        def base, do: %ExAirtable.Config.Base{
+          id: "your base ID",
+          api_key: "your api key"
+        }
+
+        def name, do: "Table Name"
+      end
+
+  ## Caching
+
+  To run a local caching server, you can include a reference to the `ExAirtable` cache server in your supervision tree, for example:
+
+      defmodule My.Application do
+        use Application
+
+        def start(_type, _args) do
+          children = [
+            # ...
+
+            # cache processes
+            {ExAirtable.Cache, table_module: MyAirtable},
+            {ExAirtable.Cache, table_module: MyOtherAirtable},
+
+            # ...
+          ]
+
+          opts = [strategy: :one_for_one, name: PhoenixCms.Supervisor]
+          Supervisor.start_link(children, opts)
+        end
+
+        # ...
+      end
 
   ## Examples
-  The codebase includes an example `Table` and `Cache` (`ExAirtable.Example.EnvTable` and `ExAirtable.Example.EnvCache`) that you can use to play around and get an idea of how the system works. 
 
-  These modules work use environment variables as their configuration. The included `Makefile` provides some quick command-line tools to run tests and a console with those environment variables pre-loaded. Simply edit the relevant environment variables in `Makefile` to point to a valid base/table name, and you'll be able to interact directly like this:
+  The codebase includes an example `Table` (`ExAirtable.Example.EnvTable`) that you can use to play around and get an idea of how the system works. This module uses environment variables as configuration. The included `Makefile` provides some quick command-line tools to run tests and a console with those environment variables pre-loaded. Simply edit the relevant environment variables in `Makefile` to point to a valid base/table name, and you'll be able to interact directly like this:
 
       # first, run `make console`, then...
       iex> EnvTable.list
       %ExAirtable.Airtable.List{records: [%Record{}, %Record{}, ...]}
 
+      # to retrieve data directly from Airtable's API...
       iex> EnvTable.retrieve("rec12345")
       %ExAirtable.Airtable.Record{}
 
-      iex> {:ok, pid} = EnvCache.start_link()
-      iex> Cache.get_all(EnvCache)
+      # to start a caching server for your table...
+      iex> ExAirtable.Cache.start_link(table_module: EnvTable, sync_rate: :timer.seconds(5))
+
+      # to get all records from the cache (without hitting the Airtable API)
+      iex> Cache.get_all(EnvTable)
+      %ExAirtable.Airtable.List{}
   """
+
+  alias ExAirtable.{Airtable, Cache}
+
+  @doc """
+  Create a record in your Airtable. See `Service.create/2` for details.
+  """
+  def create(table, %Airtable.List{} = list) do
+    apply(table, :create, [list])
+  end
+	
+  @doc """
+  Get all records from either an `ExAirtable.Table` (to query the Airtable API directly), or a corresponding `ExAirtable.Cache` (if one is started in the supervision tree). Prefer the cache if one exists.
+  """
+	def list(table) do
+    try do
+      Cache.list(table)
+    rescue
+      _ -> apply(table, :list, [])
+    end
+	end
+
+  @doc """
+  Retrieve a single record from either an `ExAirtable.Table` (to query the Airtable API directly), or a corresponding `ExAirtable.Cache` (if one is started in the supervision tree). Prefer the cache if one exists.
+  """
+	def retrieve(table, key) when is_binary(key) do
+    try do
+      Cache.retrieve(table, key)
+    rescue
+      _ -> apply(table, :retrieve, [key])
+    end
+	end
 end
