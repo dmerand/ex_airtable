d20a9ab82bae4460c1ae26298f74c36c8cb2fc42
Donald Merand <dmerand@explo.org>
2020-10-21 15:21:31 -0400

    update version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ExAirtable.MixProject do
       app: :ex_airtable,
       name: "ExAirtable",
       description: description(),
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
