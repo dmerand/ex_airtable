a73266f241e57dacfebeae0a6eae9d4537063021
Stephen Cremin <stephen@iffies.com>
2025-02-09 15:19:15 +0800

    Support new "delete multiple records" API endpoint

--- lib/ex_airtable/service.ex
+++ lib/ex_airtable/service.ex
@@ -62,7 +62,13 @@ defmodule ExAirtable.Service do
       %{"deleted" => true, "id" => "recJmmAR0IzpaekBn"}
   """
   def delete(%Config.Table{} = table, id) when is_binary(id) do
-    perform_request(table, method: :delete, url_suffix: "/#{id}")
+    perform_request(table, method: :delete, url_suffix: "/" <> id)
+  end
+
+  def delete(%Config.Table{} = table, list) when is_list(list) do
+    params = Enum.reduce(list, [], fn id, acc -> Enum.into(acc, "records[]": id) end)
+
+    perform_request(table, method: :delete, params: params)
   end
 
   @doc """
@@ -134,7 +140,7 @@ defmodule ExAirtable.Service do
       encode(table.base.id) <>
       "/" <>
       encode(table.name) <>
-      encode(suffix)
+      suffix
   end
 
   defp default_headers(%Config.Table{} = table) do
